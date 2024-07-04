dfe347d2316bd5d2097f8c83f9caa7f5931dceec
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-07-04 03:22:14 -0700

    Add type-annotated floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+'''A module that returns the whole number from a float input'''
+
+import math
+
+
+def floor(n: float) -> int:
+    '''a function that reduces a float to an integer'''
+    return math.floor(n)
