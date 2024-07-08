fad7b06df0fd30fcb0bdd74de18fdded19b93738
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-07-08 03:13:56 -0700

    Add wait_n for concurrent coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,6 +10,7 @@ import asyncio
 from typing import List
 from 0-basic_async_syntax import wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawn wait_random n times with
