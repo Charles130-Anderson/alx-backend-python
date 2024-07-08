bd04e556cdcf0354cf07b0461b70a8287bb52896
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-07-08 04:15:20 -0700

    Implement task_wait_n function

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,25 +3,20 @@
 from typing import List
 
 '''
-Take the code from wait_n and alter it into a new function task_wait_n.
-The code is nearly identical to wait_n except task_wait_random is being
-called.
+Convert wait_n to task_wait_n.
+Similar to wait_n, uses task_wait_random.
 '''
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''Runs an async function for n times and adds the results into a list'''
-    # Import the async function dynamically
+    '''Run async function n times.'''
     modified_random = __import__('3-tasks').task_wait_random
 
-    # Initialize an empty list to store delays
     delay_list = []
+    i = 0
 
-    # Loop to call modified_random and collect results
-    for _ in range(n):
+    while i < n:
         delay_list.append(await modified_random(max_delay))
+        i += 1
 
-    # Sort the delay_list in ascending order
-    delay_list.sort()
-
-    return delay_list
+    return sorted(delay_list)
