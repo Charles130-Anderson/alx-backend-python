562d1f64e710ebc91bdf9446ced0fa4e37023c45
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-07-28 02:45:38 -0700

    Add tests for memoize decorator

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""Test access_nested_map and get_json functions"""
+"""Test access_nested_map, get_json, and memoize functions"""
 
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 class TestAccessNestedMap(unittest.TestCase):
     """Test access_nested_map function"""
@@ -44,6 +44,25 @@ class TestGetJson(unittest.TestCase):
         
         self.assertEqual(get_json(test_url), test_payload)
         mock_get.assert_called_once_with(test_url)
+
+class TestMemoize(unittest.TestCase):
+    """Test memoize decorator"""
+    
+    def test_memoize(self):
+        """Test memoize returns cached result"""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            obj = TestClass()
+            self.assertEqual(obj.a_property, 42)
+            self.assertEqual(obj.a_property, 42)
+            mock_method.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()
